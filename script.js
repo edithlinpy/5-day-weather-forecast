21899f0e7e1364d08b08b0796abda08ecb71242f
Edith <edithlinpy@gmail.com>
2023-02-05 10:43:56 +0000

    add search logic

--- script.js
+++ script.js
@@ -1,19 +1,40 @@
-let city = {
-  name: "London",
-  date: "", //list.dt
-  icon: "", //list.weather.icon
-  temperature: "", //list.main.temp
-  humidity: "", //list.main.humidity
-  windSpeed: "" //list.wind.speed
-};
-let forcastData = []; // array stores 5 days forcast
+const forecastDiv = $("#forecast");
+const forcastData = []; // array stores 5 days forcast
+let cityName = "";
+
+// let city = {
+//   name: "",
+//   date: "", //list.dt
+//   icon: "", //list.weather.icon
+//   temperature: "", //list.main.temp
+//   humidity: "", //list.main.humidity
+//   windSpeed: "" //list.wind.speed
+// };
+
 let errorMsg = "";
 
  function getIconURL(icon) {
     return "http://openweathermap.org/img/w/" + icon + ".png";
   }
 
-function getWeather(cityName) {
+function setWeatherData(list) {
+
+  let city = {};
+
+  city.date = moment.unix(list.dt).format("DD-MMM-YYYY"); // convert unix timestamp to readable date format
+  city.icon = getIconURL(list.weather[0].icon);
+  city.temperature = list.main.temp;
+  city.humidity = list.main.humidity;
+  city.windSpeed = list.wind.speed;
+
+  // console.log("city:");
+  // console.log(city);  
+
+  return city;
+
+}
+
+function getWeather() {
 
   // geographical coordinates API query URL
   const gcQueryURL = "http://api.openweathermap.org/geo/1.0/direct?q="+cityName+"&appid=e7b6ba243587870fcc4406f2a4ca1ee9";
@@ -23,11 +44,6 @@ function getWeather(cityName) {
     url: gcQueryURL,
     method: "GET"
   }).then(function(response) { 
-    console.log("response:");
-    console.log(response[0]);
-    console.log(response[0].lat);
-    console.log(response[0].lon);
-
 
     // console.log(JSON.stringify(response));
 
@@ -47,7 +63,7 @@ function getWeather(cityName) {
       url: owmQueryURL,
       method: "GET"
     }).then(function(owmResponse) { 
-      console.log("owmResponse:");
+
       console.log(owmResponse);
 
       if (owmResponse.Error || owmResponse.list  === undefined) {
@@ -55,23 +71,53 @@ function getWeather(cityName) {
         return;
       }  
 
+      // add current weather
+      forcastData.push(setWeatherData(owmResponse.list[0]));
+
       // Only get the forcast data at time "00:00:00"
-      for (let i=0; i<owmResponse.list.length; i+=8) {
-        let list = owmResponse.list[i];
 
-        city.date = moment.unix(list.dt).format("DD-MMM-YYYY"); // convert unix timestamp to readable date format
-        city.icon = getIconURL(list.weather.icon);
-        city.temperature = list.main.temp;
-        city.humidity = list.main.humidity;
-        city.windSpeed = list.wind.speed;
+      for (let i=7; i<owmResponse.list.length; i+=8) {
+        console.log("i:"+i);
 
-        console.log("city:");
-        console.log(city);        
-        forcastData.push(city); // add city object to forcast array
+        forcastData.push(setWeatherData(owmResponse.list[i])); // add city object to forcast array
+
+        console.log(forcastData);
+      }
+
+      if (errorMsg === "") {
+        console.log("before showWeather");
+        showWeather();
+      } else {
+        // show error message
       }
 
     });
   });
 }
 
-getWeather(city.name);
+function showWeather() {
+
+  forcastData.forEach(cityObj => {
+    console.log("showWeather");
+    let cityInfo = `City: ${cityName} ${cityObj.date} ${cityObj.temperature} ${cityObj.windSpeed} ${cityObj.humidity}<br/>`;
+    $("#today").append(cityInfo);
+  });
+
+}
+
+// handling the search city action
+$("#search-button").on("click", function(event) {
+  // Preventing the submit button from trying to submit the form
+  event.preventDefault();
+
+    // Here we grab the text from the input box
+    cityName = $("#search-input").val().trim();
+
+    if (cityName === "") {
+      errorMsg = "Please enter a city name to search.";
+      return;
+    }
+
+    getWeather();
+
+});
